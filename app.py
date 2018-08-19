bf5768257bc874551fe3d59afe8301c349024cb6
TimothyMa <timothyma137@gmail.com>
2018-08-19 04:39:41 +0000

    index.html

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def stockgraph():
     return render_template('stockgraph.html', script=thescript, div=thediv)
 
 if __name__ == "__main__":
-    app.run(host='159.89.38.228')
+    app.run(host='0.0.0.0')
